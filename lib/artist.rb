11c213ec24a4f5eb164fd863def7596eff462ff7
Azraf Chowdhury <achowdhur@ltu.edu>
2018-11-21 08:05:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
 class Artist 
   
-  attr_accessor :name, :artist_list
+  attr_accessor :name
   
   @@all =[]
   
   def initialize(name)
     @name = name
-    
+    @@all << name
   end 
   
 end
