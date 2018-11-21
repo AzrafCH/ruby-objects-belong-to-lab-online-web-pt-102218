b7a0704ec699250fb508cd6d6423eeeda93ddb8f
Azraf Chowdhury <achowdhur@ltu.edu>
2018-11-21 08:16:15 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,9 +2,12 @@ class Post
   
   attr_accessor :title, :author
   
-  def initialize(title, author)
-    @title = title
-    @author = author
+  def title
+   @title
   end 
+  
+  def author 
+    @author
+  end
   
 end 
